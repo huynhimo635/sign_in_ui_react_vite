9e00580b8c112386d9789a53d8435cb11a03960c
huynhimo635 <huynh.nguyen.imo@gmail.com>
2023-05-26 11:01:33 +0700

    chore: make structure easier  for sass structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,14 +6,22 @@ const LoginPage = () => {
 			<Helmet title="Sign in" />
 
 			<div className="container login">
+				{/* Left side */}
 				<img
 					src="src/assets/images/login_img.jpg"
 					alt="Login Image"
 					className="login__image"
 				/>
 
+				{/* Right side */}
 				<div className="login__content">
-					<h1>test</h1>
+					<div className="login__content__items">
+						<h3 className="">Welcome to Panorama, Sign In to Continue.</h3>
+						<p className="">
+							Don&apost have an account? <span>Create an account</span> It takes
+							less than a minute.
+						</p>
+					</div>
 				</div>
 			</div>
 		</>
