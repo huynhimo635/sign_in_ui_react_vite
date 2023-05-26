c80b231ad5aea5fe7afe28e5ac61ac95d6635136
huynhimo635 <huynh.nguyen.imo@gmail.com>
2023-05-26 17:43:32 +0700

    chore: add icons source from boxicon

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,12 +15,37 @@ const LoginPage = () => {
 
 				{/* Right side */}
 				<div className="login__content">
-					<div className="login__content__items">
-						<h3 className="">Welcome to Panorama, Sign In to Continue.</h3>
+					<div className="login__content__title">
+						<h3 className="">
+							Welcome to Panorama,
+							<br />
+							Sign In to Continue.
+						</h3>
 						<p className="">
-							Don&apost have an account? <span>Create an account</span> It takes
-							less than a minute.
+							Don&lsquo;t have an account?{' '}
+							<a href="#" className="link-text">
+								Create an account
+							</a>
+							<br />
+							It takes less than a minute.
 						</p>
+					</div>
+					<div className="login__content__form">
+						<div>
+							<label htmlFor="email">Email</label>
+							<input type="text" name="email" />
+						</div>
+						<div>
+							<label htmlFor="password">Password</label>
+							<input type="password" name="password" />
+						</div>
+						<a href="#" className="link-text">
+							Forgot Password?
+						</a>
+					</div>
+					<div className="login__content__buttons">
+						<button>Sign in</button>
+						<button>Sign in via Google</button>
 					</div>
 				</div>
 			</div>
